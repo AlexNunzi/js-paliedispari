b0bd13aed5fe02856ef12ddfeab6df0d7a2a7912
Alex Nunzi <alex.nunzi@hotmail.it>
2023-03-02 17:20:28 +0100

    Aggiunto parseInt per la prompt che fa scegliere all'utente un numero da 1 a 5. Creata funzione che dato un numero restituisce true se è pari e false se è dispari.

--- PariDispari/js/main.js
+++ PariDispari/js/main.js
@@ -8,7 +8,7 @@ while(evenOdd.toLowerCase() != 'pari' && evenOdd.toLowerCase() != 'dispari'){
 console.log("L'utente ha scommesso su: " + evenOdd);
 
 while(isNaN(userNumber) || ((userNumber < 1) || (userNumber > 5))){
-    userNumber = prompt('Scrivi un numero da 1 a 5');
+    userNumber = parseInt(prompt('Scrivi un numero da 1 a 5'));
 }
 
 console.log("Il numero scelto dall'utente è: " + userNumber);
@@ -16,12 +16,27 @@ console.log("Il numero scelto dall'utente è: " + userNumber);
 const cpuNumber = randomNumber();
 console.log("Il numero scelto dal computer è: " + cpuNumber);
 
+const addition = userNumber + cpuNumber;
+console.log ('La somma dei numeri scelti è: ' + addition);
 
+const evenOrOdd = evenOddControl((userNumber + cpuNumber));
 
+console.log(evenOrOdd);
 
 
 
 function randomNumber(){
     const cpuRandomNumber = (Math.floor(Math.random() * 5)) + 1;
     return cpuRandomNumber;
+}
+
+
+function evenOddControl(number){
+    let result;
+    if(number % 2 == 0){
+        result = true;
+    } else {
+        result = false;
+    }
+    return result;
 }
