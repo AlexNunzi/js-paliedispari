dfcad6e3ce491954bfc7b4f966610553fd6a7be1
Alex Nunzi <alex.nunzi@hotmail.it>
2023-03-02 17:08:14 +0100

    Creata funzione che genera un numero casuale da 1 a 5

--- PariDispari/js/main.js
+++ PariDispari/js/main.js
@@ -5,10 +5,23 @@ while(evenOdd.toLowerCase() != 'pari' && evenOdd.toLowerCase() != 'dispari'){
     evenOdd = prompt('Scrivi "pari" o "dispari" per decidere su quale dei due vuoi scommettere');
 }
 
-console.log(evenOdd);
+console.log("L'utente ha scommesso su: " + evenOdd);
 
 while(isNaN(userNumber) || ((userNumber < 1) || (userNumber > 5))){
     userNumber = prompt('Scrivi un numero da 1 a 5');
 }
 
-console.log(userNumber);
+console.log("Il numero scelto dall'utente è: " + userNumber);
+
+const cpuNumber = randomNumber();
+console.log("Il numero scelto dal computer è: " + cpuNumber);
+
+
+
+
+
+
+function randomNumber(){
+    const cpuRandomNumber = (Math.floor(Math.random() * 5)) + 1;
+    return cpuRandomNumber;
+}
