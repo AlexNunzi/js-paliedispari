092b6f33fbd5779e9dcde9eba24901cc13f993e4
Alex Nunzi <alex.nunzi@hotmail.it>
2023-03-02 16:35:00 +0100

    Corretto collegamento a file js in index.html. Creata funzione che dando in ingresso una stringa controlla se è palindroma.

---
+++ Palindroma/js/main.js
@@ -0,0 +1,31 @@
+const userWord = prompt('Inserisci una parola e ti dirò se è palindroma');
+
+console.log("La parola inserita è: " + userWord);
+
+palindroma(userWord);
+
+
+
+
+
+
+
+
+
+
+
+function palindroma(word){
+    let reverseWord = '';
+    console.log("Lunghezza parola è: " + word.length);
+    for(i=(word.length - 1); i >= 0; i--){
+        console.log(word[i]);
+        reverseWord += word[i];
+    }
+    console.log("La parola al contrario è: " + reverseWord);
+    console.log("La parola inserita è: " + word);
+    if(word.toLowerCase() == reverseWord.toLowerCase()){
+        console.log('La parola inserita è palindroma');
+    } else{
+        console.log('La parola inserita non è palindroma');
+    }
+}
